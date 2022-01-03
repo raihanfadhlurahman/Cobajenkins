d946b9e7904d43299af649138acc41699da67141
raihanfadhlurahman <fadhraihan29@gmail.com>
2022-01-03 23:14:34 +0700

    projek 1

---
+++ Cobajenkins.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobajenkins;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cobajenkins {
+            
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String a = "Revan";
+        String b = "Lazuardi";
+        String c = a+" "+b;
+        System.out.println(c);
+        // TODO code application logic here
+    }
+    
+}
